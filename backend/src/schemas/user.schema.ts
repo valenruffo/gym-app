f1c65c0298c00863584bbd0ad3ce8a6d8fc955af
Valentino <78638982+valenruffo@users.noreply.github.com>
2025-09-09 15:51:01 +0000

    cmm6

--- backend/src/schemas/user.schema.ts
+++ backend/src/schemas/user.schema.ts
@@ -1,13 +1,30 @@
-import { Schema, Prop } from '@nestjs/mongoose';
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
 
-@Schema()
+export type UserDocument = User & Document; 
+
+@Schema({ timestamps: true })
 export class User {
-  @Prop({ required: true })
+  @Prop({ 
+    required: true,
+    trim: true,
+    maxlength: 20,
+    minlength: 3,
+  })
   name: string;
 
-  @Prop({ required: true })
+  @Prop({ 
+    unique: true, 
+    required: true,
+    isEmail: true,
+    trim: true,
+  })
   email: string;
 
-  @Prop({ required: true })
+  @Prop({ 
+    unique: true, 
+    required: true })
   password: string;
 }
+
+export const UserSchema = SchemaFactory.createForClass(User);
